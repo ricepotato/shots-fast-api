65c0c29b576b452774ad3cd8924846b6c2bbf0dc
ricepotato <sukjun40@naver.com>
2022-07-30 12:35:34 +0900

    time created, updated

--- shotsapi/models.py
+++ shotsapi/models.py
@@ -1,6 +1,15 @@
-from sqlalchemy import Column, Integer, SmallInteger, String, ForeignKey, Table
+from sqlalchemy import (
+    Column,
+    Integer,
+    SmallInteger,
+    String,
+    ForeignKey,
+    Table,
+    DateTime,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -16,7 +25,7 @@ class Tags(Base):
     __tablename__ = "tags"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    name = Column(String(50), unique=True)
 
 
 class Blob(Base):
@@ -24,10 +33,12 @@ class Blob(Base):
 
     id = Column(Integer, primary_key=True)
     shots_id = Column(Integer, ForeignKey("shots.id"))
-    name = Column(String, unique=True)
-    url = Column(String)
+    name = Column(String(50), unique=True)
+    url = Column(String(1000))
     order = Column(SmallInteger, index=True)
-    type = Column(String, index=True)
+    type = Column(String(20), index=True)
+    time_created = Column(DateTime, server_default=func.now())
+    time_updated = Column(DateTime, onupdate=func.now())
 
     owner = relationship("Shots", back_populates="images")
 
@@ -36,7 +47,10 @@ class Shots(Base):
     __tablename__ = "shots"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    name = Column(String(50), unique=True)
     custom = Column(String, index=True)
+    status = Column(String, default=None)
+    time_created = Column(DateTime, server_default=func.now())
+    time_updated = Column(DateTime, onupdate=func.now())
 
     images = relationship("Blob", back_populates="owner")
